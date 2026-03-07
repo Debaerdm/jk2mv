e4b747974d37296779fe990cd2151564338e78f6
Mathieu De Baerdemaeker <mathieu.debaerdemaeker@gmail.com>
2026-03-07 11:48:34 +0100

    refactor: extract netchan message state helpers

--- src/client/cl_net_chan.cpp
+++ src/client/cl_net_chan.cpp
@@ -5,6 +5,26 @@
 
 // TTimo: unused, commenting out to make gcc happy
 #if 1
+typedef struct netchanMessageState_s {
+	int readcount;
+	int bit;
+	qboolean oob;
+} netchanMessageState_t;
+
+static void CL_SaveMessageState( const msg_t *msg, netchanMessageState_t *state )
+{
+	state->readcount = msg->readcount;
+	state->bit = msg->bit;
+	state->oob = msg->oob;
+}
+
+static void CL_RestoreMessageState( msg_t *msg, const netchanMessageState_t *state )
+{
+	msg->readcount = state->readcount;
+	msg->bit = state->bit;
+	msg->oob = state->oob;
+}
+
 /*
 ==============
 CL_Netchan_Encode
@@ -18,29 +38,25 @@ CL_Netchan_Encode
 */
 static void CL_Netchan_Encode( msg_t *msg ) {
 	int serverId, messageAcknowledge, reliableAcknowledge;
-	int i, index, srdc, sbit;
+	int i, index;
+	netchanMessageState_t state;
 	byte key, *string;
-	qboolean soob;
 
 	if ( msg->cursize <= CL_ENCODE_START ) {
 		return;
 	}
 
-		srdc = msg->readcount;
-		sbit = msg->bit;
-		soob = msg->oob;
+	CL_SaveMessageState( msg, &state );
 
-		msg->bit = 0;
-		msg->readcount = 0;
-		msg->oob = qfalse;
+	msg->bit = 0;
+	msg->readcount = 0;
+	msg->oob = qfalse;
 
-		serverId = MSG_ReadLong(msg);
+	serverId = MSG_ReadLong(msg);
 	messageAcknowledge = MSG_ReadLong(msg);
 	reliableAcknowledge = MSG_ReadLong(msg);
 
-		msg->oob = soob;
-		msg->bit = sbit;
-		msg->readcount = srdc;
+	CL_RestoreMessageState( msg, &state );
 
 	string = (byte *)clc.serverCommands[ reliableAcknowledge & (MAX_RELIABLE_COMMANDS-1) ];
 	index = 0;
@@ -73,21 +89,16 @@ CL_Netchan_Decode
 */
 static void CL_Netchan_Decode( msg_t *msg ) {
 	int reliableAcknowledge, i, index;
+	netchanMessageState_t state;
 	byte key, *string;
-	int	srdc, sbit;
-	qboolean soob;
 
-		srdc = msg->readcount;
-		sbit = msg->bit;
-		soob = msg->oob;
+	CL_SaveMessageState( msg, &state );
 
-		msg->oob = qfalse;
+	msg->oob = qfalse;
 
 	reliableAcknowledge = MSG_ReadLong(msg);
 
-		msg->oob = soob;
-		msg->bit = sbit;
-		msg->readcount = srdc;
+	CL_RestoreMessageState( msg, &state );
 
 	string = (unsigned char *)clc.reliableCommands[ reliableAcknowledge & (MAX_RELIABLE_COMMANDS-1) ];
 	index = 0;
